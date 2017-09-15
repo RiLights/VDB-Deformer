76007b6c497c49b5cbe27ae4d4ca4b66fad977ef
Unknown <tomori.juraj@gmail.com>
2017-09-15 15:24:53 +0200

    a bit of cleaning up

--- src/vdb_deformer.C
+++ src/vdb_deformer.C
@@ -1,31 +1,3 @@
-/*
- * Copyright (c) 2017
- *  Side Effects Software Inc.  All rights reserved.
- *
- * Redistribution and use of Houdini Development Kit samples in source and
- * binary forms, with or without modification, are permitted provided that the
- * following conditions are met:
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. The name of Side Effects Software may not be used to endorse or
- *    promote products derived from this software without specific prior
- *    written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY SIDE EFFECTS SOFTWARE `AS IS' AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
- * NO EVENT SHALL SIDE EFFECTS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *----------------------------------------------------------------------------
- * The Star SOP
- */
-
 #include "vdb_deformer.h"
 
 #include <GU/GU_Detail.h>
@@ -51,20 +23,6 @@
 #include <iostream>
 
 
-
-//
-// Help is stored in a "wiki" style text file.  This text file should be copied
-// to $HOUDINI_PATH/help/nodes/sop/star.txt
-//
-// See the sample_install.sh file for an example.
-//
-
-
-///
-/// newSopOperator is the hook that Houdini grabs from this dll
-/// and invokes to register the SOP.  In this case we add ourselves
-/// to the specified operator table.
-///
 void
 newSopOperator(OP_OperatorTable *table)
 {
@@ -126,10 +84,6 @@ VDB_Deformer::myConstructor(OP_Network *net, const char *name, OP_Operator *op)
 VDB_Deformer::VDB_Deformer(OP_Network *net, const char *name, OP_Operator *op)
     : SOP_Node(net, name, op)
 {
-    //mySopFlags.setManagesDataIDs(true);
-
-
-    //myCurrPoint = -1; // To prevent garbage values from being returned
 }
 
 VDB_Deformer::~VDB_Deformer() {}
@@ -144,7 +98,6 @@ VDB_Deformer::cookMySop(OP_Context &context)
 
     gdp->clearAndDestroy();
     duplicateSource(0, context);//const GU_Detail *gdp = inputGeo(0);//duplicateSource(0, context);
-
 
 
     UT_AutoInterrupt boss("Building Deformer");
--- src/vdb_deformer.h
+++ src/vdb_deformer.h
@@ -1,31 +1,3 @@
-/*
- * Copyright (c) 2017
- *	Side Effects Software Inc.  All rights reserved.
- *
- * Redistribution and use of Houdini Development Kit samples in source and
- * binary forms, with or without modification, are permitted provided that the
- * following conditions are met:
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. The name of Side Effects Software may not be used to endorse or
- *    promote products derived from this software without specific prior
- *    written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY SIDE EFFECTS SOFTWARE `AS IS' AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
- * NO EVENT SHALL SIDE EFFECTS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *----------------------------------------------------------------------------
- * This SOP builds a star.
- */
-
 #ifndef __SOP_Star_h__
 #define __SOP_Star_h__
 
@@ -35,22 +7,21 @@
 class VDB_Deformer : public SOP_Node
 {
 public:
-    static OP_Node		*myConstructor(OP_Network*, const char *,
-							    OP_Operator *);
+    static OP_Node *myConstructor(OP_Network*, const char *, OP_Operator *);
 
     /// Stores the description of the interface of the SOP in Houdini.
     /// Each parm template refers to a parameter.
-    static PRM_Template		 myTemplateList[];
+    static PRM_Template	myTemplateList[];
     static int testUI(void *op, int, fpreal time, const PRM_Template *);
 
 protected:
-         VDB_Deformer(OP_Network *net, const char *name, OP_Operator *op);
+    VDB_Deformer(OP_Network *net, const char *name, OP_Operator *op);
     virtual ~VDB_Deformer();
 
 
     /// cookMySop does the actual work of the SOP computing, in this
     /// case, a star shape.
-    virtual OP_ERROR		 cookMySop(OP_Context &context);
+    virtual OP_ERROR cookMySop(OP_Context &context);
 
 private:
     /// The following list of accessors simplify evaluating the parameters
