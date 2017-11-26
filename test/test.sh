b63c7b4511e9fb86d22b730d74f37499e4bb2c5c
jtomori <tomori.juraj@gmail.com>
2017-11-26 20:29:24 +0100

    fixed test.sh script and Cmake for linux

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# find location of this script
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# find location of this script, move one level up
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 # set up env vars
-HOUDINI_DSO_PATH="${HOUDINI_DSO_PATH}:&:$ROOT/dso"
-HOUDINI_DSO_ERROR=1
+export HOUDINI_DSO_PATH="${HOUDINI_DSO_PATH}:&:$ROOT/dso"
+export HOUDINI_DSO_ERROR=1
+echo "DSO path "$HOUDINI_DSO_PATH
 
 # run testing scene
-houdini -foreground $ROOT/test.hip
+houdini -foreground $ROOT/test/test.hip
